99b078b6d880d72a87c10e03a7795762afd09923
Martín <magadiflo@gmail.com>
2022-10-21 23:42:11 -0500

    Revisar el valor de un input físicamente

--- src/app/intermedio-2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio-2/incrementador/incrementador.component.spec.ts
@@ -9,10 +9,10 @@ describe('Incremendator Component', () => {
     let component: IncrementadorComponent;
     let fixture: ComponentFixture<IncrementadorComponent>;
 
-    beforeEach( () => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [ IncrementadorComponent ],
-            imports: [ FormsModule ]
+            declarations: [IncrementadorComponent],
+            imports: [FormsModule]
         });
 
         fixture = TestBed.createComponent(IncrementadorComponent);
@@ -24,8 +24,17 @@ describe('Incremendator Component', () => {
         component.leyenda = 'Progreso de carga';
         fixture.detectChanges(); //* disparar la detección de cambios
 
-        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement; //* css(..), si le mandamos directo el elemento lo tomará como un elemento html. Si le anteponemos un ., se llamará como clase
         expect(elem.innerHTML).toContain('Progreso de carga');
+    });
+
+    it('Debe mostrar en el input el valor del progreso', () => {
+        component.cambiarValor(5);
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            const input: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+            expect(input.value).toBe('55');            
+        });
     });
 
 });
