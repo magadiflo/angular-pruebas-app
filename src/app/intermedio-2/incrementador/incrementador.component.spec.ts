79685ae8dd2122c5e00531abf0227a6ba088f7cb
Martín <magadiflo@gmail.com>
2022-10-21 23:48:10 -0500

    Confirmar que elementos HTML tengan los eventos deseados

--- src/app/intermedio-2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio-2/incrementador/incrementador.component.spec.ts
@@ -37,4 +37,13 @@ describe('Incremendator Component', () => {
         });
     });
 
+    it('Debe incrementar/decrementar en 5, con un click en el botón', () => {
+        const botones = fixture.debugElement.queryAll(By.css('button'));
+        botones[0].triggerEventHandler('click', null);
+        expect(component.progreso).toBe(45);
+
+        botones[1].triggerEventHandler('click', null);
+        expect(component.progreso).toBe(50);
+    });
+
 });
