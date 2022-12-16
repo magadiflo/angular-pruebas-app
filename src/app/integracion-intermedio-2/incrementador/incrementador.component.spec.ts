4d9a8d97b2051889bed64935ab6443098fc33888
Martín <magadiflo@gmail.com>
2022-12-16 12:42:54 -0500

    Se agregaron comentarios

--- src/app/integracion-intermedio-2/incrementador/incrementador.component.spec.ts
+++ src/app/integracion-intermedio-2/incrementador/incrementador.component.spec.ts
@@ -36,10 +36,18 @@ describe('Incremendator Component', () => {
 
     it('Debe mostrar en el input el valor del progreso', () => {
         component.cambiarValor(5);
-        fixture.detectChanges();
+        fixture.detectChanges(); //* Para llamar el ciclo de detección de Angular y se actualice en todos los lugares
+
+        //* Como la detección de cambio generada por fixture.detectChanges(); puede demorar, es decir
+        //* no es instantáneo, quizá tome microsergundos, entonces podríamos usar la función 
+        //* fixture.whenStable().... lo que indica es que cuando ya termine la detección de cambios 
+        //* y ya esté lista para ser evaluada podemos disparar todo el contenido que vamos a evaluar,
+        //* de esa manera las pruebas pasarán, ya que anteriormente sin esta iplementación nos mostraba
+        //* el input.value = '', es decir el expect(input.value).toBe('55'); se evaluaba antes de que 
+        //* el ciclo de detección e cambios terminara
         fixture.whenStable().then(() => {
             const input: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
-            expect(input.value).toBe('55');            
+            expect(input.value).toBe('55');
         });
     });
 
@@ -58,7 +66,7 @@ describe('Incremendator Component', () => {
 
         fixture.detectChanges();
 
-        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement; 
+        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
         expect(elem.innerHTML).toContain('45');
     });
 
