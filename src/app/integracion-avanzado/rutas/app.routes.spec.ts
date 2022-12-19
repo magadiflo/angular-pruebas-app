282f4282f25ecca86fc34db8fbcf82d6e3d40af6
Martín <magadiflo@gmail.com>
2022-12-19 10:57:16 -0500

    Solucionando importaciones luego de haber renombrado directorios

--- src/app/integracion-avanzado/rutas/app.routes.spec.ts
+++ src/app/integracion-avanzado/rutas/app.routes.spec.ts
@@ -1,11 +1,11 @@
-import { MedicosComponent } from '../../unitarias-intermedio/espias/medicos.component';
+import { MedicoComponent } from '../../integracion-intermedio-2/medico/medico.component';
 import { RUTAS } from './app.routes';
 
 describe('Rutas principales', () => {
 
     it('Debe existir la ruta /medico/:id', () => {
         expect(RUTAS).toContain(
-            { path: 'medico/:id', component: MedicosComponent }
+            { path: 'medico/:id', component: MedicoComponent }
         );
     });
 
--- src/app/integracion-avanzado/rutas/app.routes.ts
+++ src/app/integracion-avanzado/rutas/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { MedicosComponent } from '../../intermedio/espias/medicos.component';
-import { HospitalComponent } from '../../intermedio-2/hospital/hospital.component';
-import { IncrementadorComponent } from '../../intermedio-2/incrementador/incrementador.component';
+import { MedicoComponent } from '../../integracion-intermedio-2/medico/medico.component';
+import { HospitalComponent } from '../../integracion-intermedio-2/hospital/hospital.component';
+import { IncrementadorComponent } from '../../integracion-intermedio-2/incrementador/incrementador.component';
 
 export const RUTAS: Routes = [
     { path: 'hospital', component: HospitalComponent },
-    { path: 'medico/:id', component: MedicosComponent },
+    { path: 'medico/:id', component: MedicoComponent },
     { path: '**', component: IncrementadorComponent }
 ];
